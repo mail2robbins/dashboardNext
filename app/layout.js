f2222281ec55e71280eb9d5fecaa00ea351ddf39
Robin Thomas <mail2robbins@gmail.com>
2025-03-25 17:18:08 -0400

    description

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./ui/globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Next.js Admin Dashboard",
-  description: "Next.js 14 Website",
+  title: "Inventory App",
+  description: "Inventory App",
 };
 
 export default function RootLayout({ children }) {
